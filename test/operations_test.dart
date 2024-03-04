9a03d9708d762556d5552466f362c4f6838b0a91
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-04 10:55:10 +0100

    update unit test

--- test/operations_test.dart
+++ test/operations_test.dart
@@ -4,10 +4,14 @@ import 'package:test/test.dart';
 void main() {
   test('Addition should calculate correct last value in stack', () {
     final addition = AddOperation();
-    final List<num> stack = [10,5];
-
+    //int
+    final List<num> stack = [10, 5];
+    //decimal
+    final List<num> stackDec = [6.2, 1.2];
     addition.execute(stack);
+    addition.execute(stackDec);
     expect(stack.last, 15);
+    expect(stackDec.last, 7.4);
   });
 
   test('Subtraction should calculate correct last value in stack', () {
@@ -20,7 +24,7 @@ void main() {
 
   test('Multiplication should calculate correct last value in stack', () {
     final multiplication = MultiplyOperation();
-    final List<num> stack = [10,5];
+    final List<num> stack = [10, 5];
 
     multiplication.execute(stack);
     expect(stack.last, 50);
@@ -28,7 +32,7 @@ void main() {
 
   test('Division should calculate correct last value in stack', () {
     final division = DivideOperation();
-    final List<num> stack = [10,5];
+    final List<num> stack = [10, 5];
 
     division.execute(stack);
     expect(stack.last, 2);
@@ -36,14 +40,14 @@ void main() {
 
   test('Power should calculate correct last value in stack', () {
     final power = ExponentialOperation();
-    final List<num> stack = [10,5];
+    final List<num> stack = [10, 5];
     power.execute(stack);
     expect(stack.last, 100000);
   });
 
   test('Rest should calculate correct last value in stack', () {
     final rest = RestOperation();
-   final List<num> stack = [10,5];
+    final List<num> stack = [10, 5];
 
     rest.execute(stack);
     expect(stack.last, 0);
