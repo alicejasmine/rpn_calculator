5565e26c4559dc562c1c4b642d096e9ddbd84c33
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2024-03-04 18:20:53 +0100

    Added keys for testing

--- lib/CalculatorScreen.dart
+++ lib/CalculatorScreen.dart
@@ -25,7 +25,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           backgroundColor: Colors.blue.shade500),
       body: Column(
         children: <Widget>[
-          Expanded(child: _buildDisplayRow()),
+          _buildDisplayRow(),
           Expanded(
             flex: 2,
             child: GridView.count(
@@ -34,41 +34,41 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
                 _buildButton(
                     'CLEAR', Colors.red.shade300, () => onClearButtonPress()),
                 _buildButton('%', Colors.blue.shade300,
-                    () => onOperationButtonPress('%')),
+                    () => onOperationButtonPress('%'),key: Key('%')),
                 _buildButton('^', Colors.blue.shade300,
-                    () => onOperationButtonPress('^')),
+                    () => onOperationButtonPress('^'),key: Key('^')),
                 _buildButton('÷', Colors.blue.shade300,
-                    () => onOperationButtonPress('÷')),
+                    () => onOperationButtonPress('÷'),key: Key('÷')),
                 _buildButton(
-                    '7', Colors.teal.shade300, () => onNumericButtonPress('7')),
+                    '7', Colors.teal.shade300, () => onNumericButtonPress('7'), key: Key('7')),
                 _buildButton(
-                    '8', Colors.teal.shade300, () => onNumericButtonPress('8')),
+                    '8', Colors.teal.shade300, () => onNumericButtonPress('8'),key: Key('8')),
                 _buildButton(
-                    '9', Colors.teal.shade300, () => onNumericButtonPress('9')),
+                    '9', Colors.teal.shade300, () => onNumericButtonPress('9'),key: Key('9')),
                 _buildButton('x', Colors.blue.shade300,
-                    () => onOperationButtonPress('x')),
+                    () => onOperationButtonPress('x'),key: Key('x')),
                 _buildButton(
-                    '4', Colors.teal.shade300, () => onNumericButtonPress('4')),
+                    '4', Colors.teal.shade300, () => onNumericButtonPress('4'),key: Key('4')),
                 _buildButton(
-                    '5', Colors.teal.shade300, () => onNumericButtonPress('5')),
+                    '5', Colors.teal.shade300, () => onNumericButtonPress('5'),key: Key('5')),
                 _buildButton(
-                    '6', Colors.teal.shade300, () => onNumericButtonPress('6')),
+                    '6', Colors.teal.shade300, () => onNumericButtonPress('6'),key: Key('6')),
                 _buildButton('-', Colors.blue.shade300,
-                    () => onOperationButtonPress('-')),
+                    () => onOperationButtonPress('-'),key: Key('-')),
                 _buildButton(
-                    '1', Colors.teal.shade300, () => onNumericButtonPress('1')),
+                    '1',Colors.teal.shade300, () => onNumericButtonPress('1'), key: Key('1')),
                 _buildButton(
-                    '2', Colors.teal.shade300, () => onNumericButtonPress('2')),
+                    '2', Colors.teal.shade300, () => onNumericButtonPress('2'), key: Key('2')),
                 _buildButton(
-                    '3', Colors.teal.shade300, () => onNumericButtonPress('3')),
+                    '3', Colors.teal.shade300, () => onNumericButtonPress('3'),key: Key('4')),
                 _buildButton('+', Colors.blue.shade300,
-                    () => onOperationButtonPress('+')),
+                    () => onOperationButtonPress('+'),key: Key('+')),
                 _buildButton(
-                    '0', Colors.teal.shade300, () => onNumericButtonPress('0')),
+                    '0', Colors.teal.shade300, () => onNumericButtonPress('0'),key: Key('0')),
                 _buildButton(
-                    ',', Colors.teal.shade300, () => onCommaButtonPress()),
+                    ',', Colors.teal.shade300, () => onCommaButtonPress(), key: Key(',')),
                 _buildButton(
-                    'ENTER', Colors.green.shade300, () => onEnterButtonPress()),
+                    'ENTER', Colors.green.shade300, () => onEnterButtonPress(), key: Key('10')),
               ],
             ),
           ),
@@ -133,12 +133,14 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   }
 
   Widget _buildButton(
-      String text, Color backgroundColor, void Function() onPressed) {
+      String text, Color backgroundColor, void Function() onPressed, {Key? key}) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
+        key: Key(text),
         onPressed: onPressed,
         child: Text(
+
           text,
           style: TextStyle(fontSize: 15),
         ),
@@ -157,6 +159,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   Widget _buildDisplayRow() {
     return Expanded(
       child: Column(
+        key: Key('Display'),
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
@@ -165,6 +168,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               Text(
                 calculator.stack.isEmpty ? "0" : '${calculator.stack.last}',
                 style: TextStyle(fontSize: 45),
+                key: Key('result_text')
               ),
             ],
           ),
@@ -174,6 +178,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               Text(
                 '$input',
                 style: TextStyle(fontSize: 45),
+                key: Key('input_text'),
               ),
             ],
           ),
